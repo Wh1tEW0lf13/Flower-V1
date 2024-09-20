a505d2a9f465c7589a3cc42b6a63d9fc763d9cff
Wh1tEW0lf13 <miloszmis13@gmail.com>
2024-09-20 20:34:01 +0200

    Water
    Water

--- Assets/World/WorldCreator.cs
+++ Assets/World/WorldCreator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WorldCreator : MonoBehaviour
 {
     [SerializeField]
-    private GameObject ground;
+    private GameObject ground, water;
     public void CreateGrid(int xSize, int ySize)
     {
         for(int i = 0; i<xSize; i++)
@@ -16,5 +16,22 @@ public class WorldCreator : MonoBehaviour
                 grid.name = "Ground" + i + "," + j;
             }
         }
+        int upOrLeft = Random.Range(0, 2); // From down to up if 0, or from left to right if 1
+        if (upOrLeft == 0)
+        {
+            int startPosition = Random.Range(1, xSize - 1);
+            WaterCreator(startPosition, 0);
+        }
+        else
+        {
+            int startPosition = Random.Range(1, ySize - 1);
+            WaterCreator(0, startPosition);
+        }
+    }
+    public void WaterCreator(int xSize, int ySize)
+    {
+        Destroy(GameObject.Find("Ground" + xSize + "," + ySize));
+        var blue = Instantiate(water, new Vector2(xSize, ySize), Quaternion.identity);
+        blue.name = "Water" + xSize + "," + ySize;
     }
 }
